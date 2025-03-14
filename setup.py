f427e3b200c5aeb922ef49f4780f96f6fda5c806
minturtle <minturtle@users.noreply.github.com>
2025-03-14 05:32:30 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for simple_vllm package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("simple_vllm", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="simple_vllm",
+    version=read("simple_vllm", "VERSION"),
+    description="Awesome simple_vllm created by minturtle",
+    url="https://github.com/minturtle/simple-vllm/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="minturtle",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["simple_vllm = simple_vllm.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
---
+++ simple_vllm/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for simple_vllm."""
+
+from simple_vllm.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
