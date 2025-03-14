879c1cda5a70fe2584db155338bde5ab6ebbeb9e
minturtle <minseok0425@kakao.com>
2025-03-14 06:55:57 +0000

    LLM Class 생성자 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,6 @@
 import sys
+import os
 import pytest
 
 
-# each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def go_to_tmpdir(request):
-    # Get the fixture dynamically by its name.
-    tmpdir = request.getfixturevalue("tmpdir")
-    # ensure local test created packages can be imported
-    sys.path.insert(0, str(tmpdir))
-    # Chdir only for the duration of the test.
-    with tmpdir.as_cwd():
-        yield
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
