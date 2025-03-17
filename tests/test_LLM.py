f619bc0990dc5d52a517212a0d8739830f142dcf
minturtle <minseok0425@kakao.com>
2025-03-17 20:44:00 +0900

    test: 테스트 코드 수정

--- tests/test_LLM.py
+++ tests/test_LLM.py
@@ -3,16 +3,22 @@ from unittest.mock import patch
 import pytest
 
 
-def test_create_LLM_object():
-  model_id = "minseok/mymodel-1b"
-  tokenizer_id = "minseok/mymodel-1b(tokenizer_test)"
-  llm = LLM(model_name = model_id, tokenizer_name = tokenizer_id)
-  assert llm.model_name == model_id
-  assert llm.tokenizer_name == tokenizer_id
+def test_LLM_runnable_CUDA_env():
+  with patch('torch.cuda.is_available', return_value=True):
+    try:
+      generate_llm()
+    except Exception:
+      pytest.fail("CUDA환경에서는 동작 가능해야 합니다.")
+
+def test_LLM_unrunnable_CPU_env():
+ with patch('torch.cuda.is_available', return_value=False):
+    with pytest.raises(Exception):
+      generate_llm()
 
 
 def test_generate_input_prompts_many_types():
-  llm = generate_llm()
+  with patch('torch.cuda.is_available', return_value=True):
+    llm = generate_llm()
 
   try:
     llm.generate("test prompt")
@@ -26,19 +32,6 @@ def test_generate_input_prompts_many_types():
 
   with pytest.raises(TypeError):
     llm.generate(123123)
-
-def test_LLM_runnable_CUDA_env():
-  with patch('torch.cuda.is_available', return_value=True):
-    try:
-      generate_llm()
-    except Exception:
-      pytest.fail("CUDA환경에서는 동작 가능해야 합니다.")
-
-def test_LLM_unrunnable_CPU_env():
- with patch('torch.cuda.is_available', return_value=False):
-    with pytest.raises(Exception):
-      generate_llm()
-
 
 def generate_llm(
   model_name = "minseok/mymodel-1b",
